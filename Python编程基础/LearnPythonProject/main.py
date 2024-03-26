1fa03b17f123f62535a660a662726b1507989d13
YYZYHS <yangyizhen17869@outlook.com>
2024-03-26 23:55:00 +0800

    2024年3月26日学习笔记提交
    - 黑马程序员Git全套教程
    	- 05 git环境配与安装
    	- 06 获取本地仓库
    	- 07 git常用指令
    - Python
    	- 8. Python中的注释
    	- 9. Python中的缩进与本章总结
    	- 10. 章节习题讲解

---
+++ Python编程基础/LearnPythonProject/main.py
@@ -0,0 +1,6 @@
+def add_numbers(num1, num2):
+    return num1 + num2
+
+# 使用示例
+result = add_numbers(3, 5)
+print(result)  # 输出：8, 3 + 5 = 8
---
+++ Python编程基础/LearnPythonProject/示例2-2 使用input函数.py
@@ -0,0 +1,9 @@
+#  input函数的使用
+name = input("请输入你的名字：")
+print("你好，" + name)
+
+#  输入数字并转换为整数
+age = input("请输入你的年龄：")
+print("你的年龄是：" + age)  # 输出年龄
+age = int(age)  # 将输入的字符串转换为整数
+print("你的年龄是：" + str(age))  # 输出年龄
