cdc37d82d2de63e89f48b306989e0029b76ab53b
YYZYHS <yangyizhen17869@outlook.com>
2024-06-25 15:29:46 +0800

    2024年6月25日 15点29分
    1、完成单链表_C语言版_有头结点的代码编写

--- C++/练习代码/单链表_C语言版_有头结点/main.c
+++ C++/练习代码/单链表_C语言版_有头结点/main.c
@@ -21,13 +21,28 @@ typedef struct LNode
 /*2、链表操作函数声明*/
 bool initList(LinkList* L);		//链表初始化
 LNode* createNewNode(ElemType e);	//创建一个新结点
-
+void printList(LinkList L);		//打印链表
+LNode* getElem(LinkList L, int i);	//按位查找
+int locateElem(LinkList L, ElemType e);	//按值查找
 bool initListByHead(LinkList* L); //头插法建立链表
 bool initListTail(LinkList* L); //尾插法建立链表
+bool insertList(LinkList L, int i, ElemType e);	//按位插入
+bool insertListByForward(LinkList L, int i, ElemType e);	//按位插入---前插法
+bool deleteElem(LinkList L, int i, ElemType* e);	//按位删除
+bool destroyList(LinkList* L);	//销毁链表
+bool isEmpty(LinkList L);		//判空
+int length(LinkList L);			//求表长
+
+
 
 /*3、链表的常用函数：打印输出*/
 void printList(LinkList L)
 {
+	if (NULL == L)
+	{
+		printf("链表不存在\n");
+		return;
+	}
 	printf("链表中的元素为: ");
 	LNode* p = L->next;			//p指向第一个结点
 	while (p != NULL)			//p不为空,即p指向的结点存在
@@ -48,7 +63,6 @@ bool initList(LinkList* L)
 	{
 		return false;
 	}
-	(*L)->data = 0;							//将头结点的数据域利用起来，记录链表的长度
 	(*L)->next = NULL;						//头结点的指针域置空
 	return true;
 }
@@ -74,7 +88,6 @@ bool initListByHead(LinkList* L)
 	{
 		return false;
 	}
-	(*L)->data = 0;							//将头结点的数据域利用起来，记录链表的长度
 	(*L)->next = NULL;						//头结点的指针域置空
 
 	while (true)							//循环输入元素的值
@@ -93,7 +106,6 @@ bool initListByHead(LinkList* L)
 		}
 		newNode->next = (*L)->next;			//新结点的指针域指向头结点的下一个结点
 		(*L)->next = newNode;				//头结点的指针域指向新结点
-		(*L)->data++;						//链表长度加1
 	}
 	return true;
 }
@@ -105,7 +117,6 @@ bool initListTail(LinkList* L)
 	{
 		return false;
 	}
-	(*L)->data = 0;							//将头结点的数据域利用起来，记录链表的长度
 	(*L)->next = NULL;						//头结点的指针域置空
 
 	LNode* tail = *L;						//tail指向头结点
@@ -125,7 +136,6 @@ bool initListTail(LinkList* L)
 		}
 		tail->next = newNode;				//尾结点的指针域指向新结点
 		tail = newNode;						//尾结点指向新结点
-		(*L)->data++;						//链表长度加1
 	}
 	return true;
 }
@@ -133,12 +143,12 @@ bool initListTail(LinkList* L)
 // 按位查找
 LNode* getElem(LinkList L, int i)
 {
-	if (i < 0 || i > L->data)	//判断i的合法性
+	if (i < 0)	//判断i的合法性
 	{
 		return NULL;
 	}
-	LNode* p = L->next;			//p指向第一个结点
-	int j = 1;					//j为计数器,链表位序从1开始
+	LNode* p = L;				//p指向第一个结点
+	int j = 0;					//j为计数器,代表当前结点的位序
 	while (p != NULL && j < i)	//循环查找第i个结点
 	{
 		p = p->next;			//p指向下一个结点
@@ -150,87 +160,218 @@ LNode* getElem(LinkList L, int i)
 // 按值查找
 int locateElem(LinkList L, ElemType e)
 {
-	LNode* p = L->next;			//p指向第一个结点
-	int i = 1;					//i为计数器,链表位序从1开始
+	LNode* p = L->next;					//p指向第一个结点
+	int i = 1;							//i为计数器,链表位序从1开始
+	// 因为 && 运算符的规则是, 只要有一个为假,就不再执行后面的判断,所以当这里的p != NULL成立时,才会执行后面的p->data != e
 	while (p != NULL && p->data != e)	//循环查找数据域为e的结点
 	{
-		p = p->next;			//p指向下一个结点
-		i++;					//计数器加1
+		p = p->next;					//p指向下一个结点
+		i++;							//计数器加1
 	}
-	if (p == NULL)				//查找失败
+	if (p == NULL)						//查找失败
 	{
 		return 0;
 	}
-	return i;					//返回数据域为e的结点的位序
+	return i;							//返回数据域为e的结点的位序
 }
 
 /*6、链表的基本操作：插入*/
-// 按位插入 --- 前插法
-bool insertListByForward(LinkList L, int i, ElemType e)
+// 在指定结点之后插入新结点
+bool insertNextNode(LNode* p, ElemType e)
 {
-	// 思路：要插入到第i个位置，且是插在某个结点的前面，所以要找到第i个结点，并将第i-1个结点的指针域指向新结点
-
-	// 判断i插入位置的合法性
-	if (i < 1 || i > L->data + 1)	
+	// 判断结点是否为空
+	if (NULL == p)
 	{
-		return false;
-	}
-
-	// 寻找第i-1个结点
-	LNode* p = getElem(L, i - 1);
-	if(NULL == p)	// 查找失败
-	{
+		printf("结点为空\n");
 		return false;
 	}
 
 	// 创建一个新结点
 	LNode* newNode = createNewNode(e);
-	if(NULL == newNode)	// 内存分配失败
+	if (NULL == newNode)	//内存分配失败
 	{
+		printf("内存分配失败\n");
 		return false;
 	}
-	
-	// 前插法插入新结点
-	newNode->next = p->next;
-	p->next = newNode;
 
-	// 链表长度加1
-	L->data++;
+	// 后插法插入新结点
+	newNode->next = p->next;	//新结点的指针域指向p的下一个结点
+	p->next = newNode;		//p的指针域指向新结点
+	return true;
+}
+
+// 在指定结点之前插入新结点
+bool insertPriorNode(LinkList L, LNode* p, ElemType e)
+{
+	// 判断结点是否为空
+	if (NULL == p)
+	{
+		printf("结点为空\n");
+		return false;
+	}
+
+	/*
+	// 前插法插入新结点方法一，时间复杂度O(n)
+	LNode* q = L;	//q指向头结点
+	while (q->next != p)	//查找p的前一个结点
+	{
+		q = q->next;
+	}
+	if(NULL == q)	//查找失败
+	{
+		printf("查找前置结点失败\n");
+		return false;
+	}
+	// 创建一个新结点
+	LNode *newNode = createNewNode(e);
+	if (NULL == newNode)	//内存分配失败
+	{
+		printf("内存分配失败\n");
+		return false;
+	}
+	newNode->next = p;	//新结点的指针域指向p
+	q->next = newNode;	//q的指针域指向新结点
+	return true;
+	*/
+
+	// 前插法插入新结点方法二，时间复杂度O(1)
+	// 创建一个新结点
+	LNode* newNode = createNewNode(e);
+	if (NULL == newNode)	//内存分配失败
+	{
+		printf("内存分配失败\n");
+		return false;
+	}
+	newNode->next = p->next;	//新结点的指针域指向p的下一个结点
+	p->next = newNode;		//p的指针域指向新结点
+	ElemType temp = p->data;	//交换数据域
+	p->data = newNode->data;
+	newNode->data = temp;
 	return true;
 }
 
 // 按位插入 --- 后插法
-bool insertListByBack(LinkList L, int i, ElemType e)
+bool insertList(LinkList L, int i, ElemType e)
 {
-	if (i < 1 || i > L->data + 1)	//判断i的合法性
+	printf("操作：在单链表的第%d个位置插入元素%d\n", i, e);
+	// 后插法思路:先找到第i-1个结点,然后在第i-1个结点后插入新结点
+	// 判断插入位置i的合法性
+	if (i < 1)
 	{
+		printf("插入位置不合法\n");
 		return false;
 	}
-	LNode* p = getElem(L, i);		//查找第i个结点
+
+	// 查找第i个结点
+	LNode* p = getElem(L, i - 1);
 	if (p == NULL)					//查找失败
 	{
+		printf("查找第%d个结点失败\n", i);
 		return false;
 	}
-	LNode* newNode = createNewNode(e);	//创建一个新结点
-	if (newNode == NULL)			//内存分配失败
-	{
-		return false;
-	}
-	newNode->next = p->next;		//新结点的指针域指向第i+1个结点
-	p->next = newNode;			//第i个结点的指针域指向新结点
-	L->data++;					//链表长度加1
-	return true;
+
+	// 调用后插法插入新结点函数
+	return insertNextNode(p, e);
 }
+
+// 按位插入 --- 前插法
+bool insertListByForward(LinkList L, int i, ElemType e)
+{
+	printf("操作：在单链表的第%d个位置插入元素%d\n", i, e);
+	// 前插法思路:先找到第i个结点,然后在第i个结点前插入新结点
+	// 判断插入位置i的合法性
+	if (i < 1)
+	{
+		printf("插入位置不合法\n");
+		return false;
+	}
+
+	// 查找第i个结点
+	LNode* p = getElem(L, i);
+	if (p == NULL)					//查找失败
+	{
+		printf("查找第%d个结点失败\n", i);
+		return false;
+	}
+
+	// 调用前插法插入新结点函数
+	return insertPriorNode(L, p, e);
+
+	/*
+		前插法有其局限性：如果插入位置是表长+1，前插法无法实现
+		因为在定位时，表长+1的位置会被定位到NULL，此时无法找到前一个结点
+		除非在定位时，判断插入位置是否为表长+1，如果是，就直接插入到表尾，但这样会增加代码的复杂度
+	*/
+}
+
 
 /*7、链表的基本操作：删除*/
 // 按位删除
+bool deleteElem(LinkList L, int i, ElemType* e)
+{
+	// 判断删除位置i的合法性
+	if (i < 1)
+	{
+		printf("删除位置不合法\n");
+		return false;
+	}
+
+	// 查找第i-1个结点
+	LNode* p = getElem(L, i - 1);
+	if (p == NULL || p->next == NULL)	//查找失败
+	{
+		printf("查找第%d个结点失败\n", i - 1);
+		return false;
+	}
+
+	// 删除第i个结点
+	LNode* q = p->next;		//q指向第i个结点
+	if (NULL != e)
+	{
+		*e = q->data;			//保存第i个结点的数据域
+	}
+	p->next = q->next;		//删除第i个结点
+	free(q);				//释放第i个结点的内存
+	return true;
+}
 
 // 按值删除
+bool deleteElemByValue(LinkList L, ElemType e)
+{
+	// 查找值为e的结点
+	LNode* p = L;	//p指向头结点
+	while (p->next != NULL && p->next->data != e)	//循环查找数据域为e的结点
+	{
+		p = p->next;	//p指向下一个结点
+	}
+	if (p->next == NULL)	//查找失败
+	{
+		printf("查找值为%d的结点失败\n", e);
+		return false;
+	}
+
+	// 删除值为e的结点
+	LNode* q = p->next;		//q指向值为e的结点
+	p->next = q->next;		//删除值为e的结点
+	free(q);				//释放值为e的结点的内存
+	return true;
+}
+
 
 /*8、链表的基本操作：销毁链表*/
 bool destroyList(LinkList* L)
 {
+	// 释放所有结点的内存
+	while (NULL != (*L)->next)
+	{
+		deleteElem(*L, 1, NULL);
+	}
 
+	// 释放头结点的内存
+	free(*L);
+	*L = NULL;
+
+	return true;
 }
 
 /*9、链表的常用函数：判空*/
@@ -240,14 +381,8 @@ bool isEmpty(LinkList L)
 }
 
 /*10、链表的常用函数：求表长*/
-// 基于本案例特殊情况下，求表长的时间复杂度可以为O(1)
-int length1(LinkList L)
-{
-	return L->data;		// 头结点的数据域存储的是链表的长度
-}
 
-// 基于一般情况下，求表长的时间复杂度为O(n)
-int length2(LinkList L)
+int length(LinkList L)
 {
 	int len = 0;		//初始化长度为0
 	LNode* p = L->next;
@@ -261,12 +396,42 @@ int length2(LinkList L)
 
 int main()
 {
-	LinkList L;		//声明一个链表
-	initListTail(&L);
+	LinkList L;				//声明一个链表
+	initListTail(&L);		//初始化链表
+	printList(L);			//打印链表
 
+	insertListByForward(L, 1, 100);		//在第一个位置插入100
+	printList(L);			//打印链表
+
+	insertListByForward(L, 2, 200);		//在第二个位置插入200
+	printList(L);			//打印链表
+
+	insertList(L, length(L) + 1, 300);		//在最后一个位置插入300
 	printList(L);	//打印链表
-	printf("链表的长度为：%d\n", length1(L));	//输出链表的长度
-	printf("链表的长度为：%d\n", length2(L));	//输出链表的长度
+
+	// 按值查找测试
+	int pos = locateElem(L, 300);	//查找300的位置
+	printf("300的位置为：%d\n", pos);
+
+	printf("链表的长度为：%d\n", length(L));	//输出链表的长度
+
+	// 按位删除测试
+	deleteElem(L, 1, NULL);	//删除第一个结点
+	printList(L);	//打印链表
+
+	deleteElem(L, 3, NULL);	//删除中间结点
+	printList(L);	//打印链表
+
+	deleteElem(L, length(L), NULL);	//删除最后一个结点
+	printList(L);	//打印链表
+
+	// 按值删除测试
+	deleteElemByValue(L, 200);	//删除值为200的结点
+	printList(L);	//打印链表
+	deleteElemByValue(L, 5959);	//删除不存在的结点
+
+	destroyList(&L);	//销毁链表
+	printList(L);	//打印链表
 
 	return 0;
 }
